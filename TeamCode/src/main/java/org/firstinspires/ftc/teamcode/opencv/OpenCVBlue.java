ba386520d389d073a0a2e4fdd01bf45b7d1390a9
squid2010 <sumerchaudhary1@gmail.com>
2024-01-10 20:23:55 -0500

    Fixed Github

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/OpenCVBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/OpenCVBlue.java
@@ -1,11 +1,5 @@
-package tele;
+package org.firstinspires.ftc.teamcode.opencv;
 
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
@@ -15,88 +9,43 @@ import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.imgproc.Moments;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@TeleOp(name = "OpenCV Red")
 
-public class OpenCVRed extends LinearOpMode {
+public class OpenCVBlue{
 
     //Focal points
-    double cX = 0;
-    double cY = 0;
-    double width = 0;
+    public static double cX = 0;
+    public static double cY = 0;
 
-    private OpenCvCamera controlHubCam;  // Use OpenCvCamera class from FTC SDK
-    private static final int CAMERA_WIDTH = 640; // width  of wanted camera resolution
-    private static final int CAMERA_HEIGHT = 480; // height of wanted camera resolution
+    public static double width = 0;
+
 
     // Calculate the distance using the formula
     public static final double objectWidthInRealWorldUnits = 3.7;  //For future devs, replace with the actual width of the object in real-world units
     public static final double focalLength = 746.153846;  // Replace with the focal length of the camera in pixels (Shouldn't change if we are using the same camera)
 
 
-    @Override
-    public void runOpMode() {
-
-        //Dashboard not needed, yet recommended for viewing
-        initOpenCV();
-        FtcDashboard dashboard = FtcDashboard.getInstance();
-        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
-        FtcDashboard.getInstance().startCameraStream(controlHubCam, 30);
-
-
-        waitForStart();
-
-        while (opModeIsActive()) {
-            telemetry.addData("Coordinate", "(" + (int) cX + ", " + (int) cY + ")");
-            telemetry.addData("Distance in Inch", (getDistance(width)));
-            telemetry.update();
-
-            // The OpenCV pipeline automatically processes frames and handles detection
-        }
-
-        // Release resources
-        controlHubCam.stopStreaming();
-    }
-
-    private void initOpenCV() {
-
-        // Create an instance of the camera
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-
-        // Use OpenCvCameraFactory class from FTC SDK to create camera instance
-        controlHubCam = OpenCvCameraFactory.getInstance().createWebcam(
-                hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
-
-        controlHubCam.setPipeline(new redBlobDetectionPipeline());
-
-        controlHubCam.openCameraDevice(); //I don't know why this is deprecated
-        controlHubCam.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT);
-    }
-    class redBlobDetectionPipeline extends OpenCvPipeline {
+    public static class blueBlobDetectionPipeline extends OpenCvPipeline {
         @Override
         public Mat processFrame(Mat input) {
             // Preprocess the frame to detect red regions
-            Mat redMask = preprocessFrame(input);
+            Mat blueMask = preprocessFrame(input);
 
-            // Find contours of the detected red regions
+            // Find contours of the detected blue regions
             List<MatOfPoint> contours = new ArrayList<>();
             Mat hierarchy = new Mat();
-            Imgproc.findContours(redMask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+            Imgproc.findContours(blueMask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
-            // Find the largest red contour (blob)
+            // Find the largest blue contour (blob)
             MatOfPoint largestContour = findLargestContour(contours);
 
-            if (largestContour != null) {
-                // Draw a red outline around the largest detected object
-                Imgproc.drawContours(input, contours, contours.indexOf(largestContour), new Scalar(255, 0, 0), 2);
+            if (largestContour != null && calculateWidth(largestContour) > 95) {
+                // Draw a blue outline around the largest detected object
+                Imgproc.drawContours(input, contours, contours.indexOf(largestContour), new Scalar(0, 0, 255), 2);
                 // Calculate the width of the bounding box
                 width = calculateWidth(largestContour);
 
@@ -116,8 +65,11 @@ public class OpenCVRed extends LinearOpMode {
                 Imgproc.putText(input, label, new Point(cX + 10, cY), Imgproc.FONT_HERSHEY_COMPLEX, 0.5, new Scalar(0, 255, 0), 2);
                 Imgproc.circle(input, new Point(cX, cY), 5, new Scalar(0, 255, 0), -1);
 
-            }
 
+            }
+            if(largestContour != null && calculateWidth(largestContour)<95){
+                cX = -10;
+            }
             return input;
         }
 
@@ -125,19 +77,19 @@ public class OpenCVRed extends LinearOpMode {
             Mat hsvFrame = new Mat();
             Imgproc.cvtColor(frame, hsvFrame, Imgproc.COLOR_BGR2HSV);
 
-            //Lower and upper red bounds.
-            Scalar lowerRed = new Scalar(100, 100, 100);
-            Scalar upperRed = new Scalar(180, 255, 255);
+            //In HSV form. Keep Saturation and Value the same for different colors
+            Scalar lowerBlue = new Scalar(0, 50, 50);
+            Scalar upperBlue = new Scalar(25, 255, 255);
 
             //Create a submatrix masking the contour
-            Mat redMask = new Mat();
-            Core.inRange(hsvFrame, lowerRed, upperRed, redMask);
+            Mat blueMask = new Mat();
+            Core.inRange(hsvFrame, lowerBlue, upperBlue, blueMask);
 
             Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
-            Imgproc.morphologyEx(redMask, redMask, Imgproc.MORPH_OPEN, kernel);
-            Imgproc.morphologyEx(redMask, redMask, Imgproc.MORPH_CLOSE, kernel);
+            Imgproc.morphologyEx(blueMask, blueMask, Imgproc.MORPH_OPEN, kernel);
+            Imgproc.morphologyEx(blueMask, blueMask, Imgproc.MORPH_CLOSE, kernel);
 
-            return redMask;
+            return blueMask;
         }
 
         private MatOfPoint findLargestContour(List<MatOfPoint> contours) {
@@ -162,7 +114,7 @@ public class OpenCVRed extends LinearOpMode {
         }
 
     }
-    private static double getDistance(double width){
+    public static double getDistance(double width){
         //Uses distance using focal length. Yay physics and optics
         double distance = (objectWidthInRealWorldUnits * focalLength) / width;
         return distance;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/OpenCVRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/OpenCVRed.java
@@ -1,11 +1,5 @@
-package tele;
+package org.firstinspires.ftc.teamcode.opencv;
 
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
@@ -15,87 +9,41 @@ import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.imgproc.Moments;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@TeleOp(name = "OpenCV Blue")
 
-public class OpenCVBlue extends LinearOpMode {
+public class OpenCVRed{
 
     //Focal points
-    double cX = 0;
-    double cY = 0;
-    double width = 0;
+    public static double cX = 0;
+    public static double cY = 0;
+    public static double width = 0;
 
-    private OpenCvCamera controlHubCam;  // Use OpenCvCamera class from FTC SDK
-    private static final int CAMERA_WIDTH = 640; // width  of wanted camera resolution
-    private static final int CAMERA_HEIGHT = 480; // height of wanted camera resolution
 
     // Calculate the distance using the formula
     public static final double objectWidthInRealWorldUnits = 3.7;  //For future devs, replace with the actual width of the object in real-world units
     public static final double focalLength = 746.153846;  // Replace with the focal length of the camera in pixels (Shouldn't change if we are using the same camera)
 
 
-    @Override
-    public void runOpMode() {
-
-        //Dashboard not needed, yet recommended for viewing
-        initOpenCV();
-        FtcDashboard dashboard = FtcDashboard.getInstance();
-        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
-        FtcDashboard.getInstance().startCameraStream(controlHubCam, 30);
-
-
-        waitForStart();
-
-        while (opModeIsActive()) {
-            telemetry.addData("Coordinate", "(" + (int) cX + ", " + (int) cY + ")");
-            telemetry.addData("Distance in Inch", (getDistance(width)));
-            telemetry.update();
-
-            // The OpenCV pipeline automatically processes frames and handles detection
-        }
-
-        // Release resources
-        controlHubCam.stopStreaming();
-    }
-
-    private void initOpenCV() {
-
-        // Create an instance of the camera
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-
-        // Use OpenCvCameraFactory class from FTC SDK to create camera instance
-        controlHubCam = OpenCvCameraFactory.getInstance().createWebcam(
-                hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
-
-        controlHubCam.setPipeline(new blueBlobDetectionPipeline());
-
-        controlHubCam.openCameraDevice(); //I don't know why this is deprecated
-        controlHubCam.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT);
-    }
-    class blueBlobDetectionPipeline extends OpenCvPipeline {
+    public static class RedBlobDetectionPipeline extends OpenCvPipeline {
         @Override
         public Mat processFrame(Mat input) {
-            // Preprocess the frame to detect blue regions
-            Mat blueMask = preprocessFrame(input);
+            // Preprocess the frame to detect red regions
+            Mat RedMask = preprocessFrame(input);
 
-            // Find contours of the detected blue regions
+            // Find contours of the detected Red regions
             List<MatOfPoint> contours = new ArrayList<>();
             Mat hierarchy = new Mat();
-            Imgproc.findContours(blueMask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+            Imgproc.findContours(RedMask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
-            // Find the largest blue contour (blob)
+            // Find the largest Red contour (blob)
             MatOfPoint largestContour = findLargestContour(contours);
 
-            if (largestContour != null) {
-                // Draw a blue outline around the largest detected object
+            if (largestContour != null && calculateWidth(largestContour) > 95) {
+                // Draw a Red outline around the largest detected object
                 Imgproc.drawContours(input, contours, contours.indexOf(largestContour), new Scalar(255, 0, 0), 2);
                 // Calculate the width of the bounding box
                 width = calculateWidth(largestContour);
@@ -116,8 +64,11 @@ public class OpenCVBlue extends LinearOpMode {
                 Imgproc.putText(input, label, new Point(cX + 10, cY), Imgproc.FONT_HERSHEY_COMPLEX, 0.5, new Scalar(0, 255, 0), 2);
                 Imgproc.circle(input, new Point(cX, cY), 5, new Scalar(0, 255, 0), -1);
 
-            }
 
+            }
+            if(largestContour != null && calculateWidth(largestContour)<95){
+                cX = -10;
+            }
             return input;
         }
 
@@ -126,18 +77,18 @@ public class OpenCVBlue extends LinearOpMode {
             Imgproc.cvtColor(frame, hsvFrame, Imgproc.COLOR_BGR2HSV);
 
             //In HSV form. Keep Saturation and Value the same for different colors
-            Scalar lowerBlue = new Scalar(0, 50, 50);
-            Scalar upperBlue = new Scalar(25, 255, 255);
+            Scalar lowerRed = new Scalar(100, 100, 100);
+            Scalar upperRed = new Scalar(180, 255, 255);
 
             //Create a submatrix masking the contour
-            Mat blueMask = new Mat();
-            Core.inRange(hsvFrame, lowerBlue, upperBlue, blueMask);
+            Mat RedMask = new Mat();
+            Core.inRange(hsvFrame, lowerRed, upperRed, RedMask);
 
             Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
-            Imgproc.morphologyEx(blueMask, blueMask, Imgproc.MORPH_OPEN, kernel);
-            Imgproc.morphologyEx(blueMask, blueMask, Imgproc.MORPH_CLOSE, kernel);
+            Imgproc.morphologyEx(RedMask, RedMask, Imgproc.MORPH_OPEN, kernel);
+            Imgproc.morphologyEx(RedMask, RedMask, Imgproc.MORPH_CLOSE, kernel);
 
-            return blueMask;
+            return RedMask;
         }
 
         private MatOfPoint findLargestContour(List<MatOfPoint> contours) {
@@ -162,7 +113,7 @@ public class OpenCVBlue extends LinearOpMode {
         }
 
     }
-    private static double getDistance(double width){
+    public static double getDistance(double width){
         //Uses distance using focal length. Yay physics and optics
         double distance = (objectWidthInRealWorldUnits * focalLength) / width;
         return distance;
