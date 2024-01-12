4b808673597aedde611ab022455ed37fe5b89760
MasterPotter <zoeb.m.izzi@gmail.com>
2024-01-11 22:05:20 -0500

    commission

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoBlueFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoBlueFar.java
@@ -13,34 +13,42 @@ public class AutoBlueFar extends LinearOpMode{
     public DcMotor leftBackDrive    = null;
     public DcMotor rightBackDrive   = null;
     public DcMotorEx leftEncoder    = null;
-    public DcMotorEx rightEncoder   = null;
-    public DcMotorEx backEncoder    = null;
+    public DcMotorEx rightEncoder  = null;
     @Override
 
-    public void runOpMode(){
+    public void runOpMode() throws InterruptedException {
         leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFront");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFront");
         leftBackDrive = hardwareMap.get(DcMotor.class, "leftRear");
         rightBackDrive = hardwareMap.get(DcMotor.class, "rightRear");
         leftEncoder = hardwareMap.get(DcMotorEx.class, "leftFront");
         rightEncoder = hardwareMap.get(DcMotorEx.class, "rightFront");
-        backEncoder = hardwareMap.get(DcMotorEx.class, "leftRear");
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
-
-        leftEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         waitForStart();
         if (opModeIsActive()) {
-            double t = 0;
-            while (leftEncoder.getCurrentPosition() < 75000){
-                leftFrontDrive.setPower(0.5);
+            for (long stop = System.nanoTime()+ TimeUnit.SECONDS.toNanos(1); stop>System.nanoTime();) {
+                leftFrontDrive.setPower(0.45);
                 rightFrontDrive.setPower(0.5);
-                leftBackDrive.setPower(0.5);
+                leftBackDrive.setPower(0.45);
                 rightBackDrive.setPower(0.5);
+                telemetry.addData("Time: ", stop);
+                telemetry.update();
             }
-        }
+            Thread.sleep(4000);
+            leftEncoder.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+            rightEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            for (int i = 0; i < 999999999; i++){
+                if (rightEncoder.getCurrentPosition() < 75000){
+                    rightFrontDrive.setPower(0.5);
+                    leftBackDrive.setPower(0.45);
+                    rightBackDrive.setPower(0.5);
+                    leftFrontDrive.setPower(0.45);
+                    telemetry.addData("leftEnc", leftEncoder.getCurrentPosition());
+                    telemetry.update();
+                }
+            }
         }
 
     }
+}
