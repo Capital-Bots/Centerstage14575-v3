30cec713c626c1fc275e749be6c7e0b47bc1e7df
MasterPotter <zoeb.m.izzi@gmail.com>
2024-01-11 21:21:34 -0500

    auto work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoBlueFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoBlueFar.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Auto;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,6 +12,9 @@ public class AutoBlueFar extends LinearOpMode{
     public DcMotor rightFrontDrive  = null;
     public DcMotor leftBackDrive    = null;
     public DcMotor rightBackDrive   = null;
+    public DcMotorEx leftEncoder    = null;
+    public DcMotorEx rightEncoder   = null;
+    public DcMotorEx backEncoder    = null;
     @Override
 
     public void runOpMode(){
@@ -18,17 +22,25 @@ public class AutoBlueFar extends LinearOpMode{
         rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFront");
         leftBackDrive = hardwareMap.get(DcMotor.class, "leftRear");
         rightBackDrive = hardwareMap.get(DcMotor.class, "rightRear");
+        leftEncoder = hardwareMap.get(DcMotorEx.class, "leftFront");
+        rightEncoder = hardwareMap.get(DcMotorEx.class, "rightFront");
+        backEncoder = hardwareMap.get(DcMotorEx.class, "leftRear");
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        leftEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         waitForStart();
         if (opModeIsActive()) {
-            for (long stop = System.nanoTime()+ TimeUnit.SECONDS.toNanos(1); stop>System.nanoTime();) {
+            double t = 0;
+            while (leftEncoder.getCurrentPosition() < 75000){
                 leftFrontDrive.setPower(0.5);
                 rightFrontDrive.setPower(0.5);
                 leftBackDrive.setPower(0.5);
                 rightBackDrive.setPower(0.5);
             }
         }
+        }
 
     }
-}
