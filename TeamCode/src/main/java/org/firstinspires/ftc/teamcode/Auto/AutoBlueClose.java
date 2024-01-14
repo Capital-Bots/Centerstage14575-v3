c977bb8461582088bea1a1c99899460933be8bb9
squid2010 <sumerchaudhary1@gmail.com>
2024-01-14 08:53:33 -0500

    Added Blue Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoBlueClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoBlueClose.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.opencv.OpenCVBlue;
 import org.firstinspires.ftc.teamcode.opencv.OpenCVBlue.blueBlobDetectionPipeline;
